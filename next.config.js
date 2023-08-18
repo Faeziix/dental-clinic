d892c51251edb94a5274e25bbbc9360fad6c7960
Faez <faez@psight.io>
2023-08-18 18:06:01 +0400

    chore(next.config.js): add typescript configuration to ignore build errors feat(next.config.js): add remotePatterns configuration for images to support fetching images from a specific hostname and pathname

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "instagram.ffjr7-1.fna.fbcdn.net",
         port: "",
-        pathname: "/v/t51.2885-15/**", // Replace this with the actual pathname pattern for your images
+        pathname: "/v/t51.2885-15/**", // Replace this with the actual pathname
+        // pattern for your images
       },
     ],
   },
