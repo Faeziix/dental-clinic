3773ef3e04a46c462dc08605c731849a4a367f70
Faez <faez@psight.io>
2023-08-21 20:20:50 +0400

    Creating contact page for mobile and desktop

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,85 +1,87 @@
 import React from "react";
 import Image from "next/image";
-import InstagramIcon from "@/public/instagram icon.svg";
-import PhoneIcon from "@/public/footer/phone.svg";
-import ClockIcon from "@/public/footer/clock.svg";
+import InstagramIcon from "@/public/InstagramIcon.png";
+import WhatsAppIcon from "@/components/icons/WhatsappIcon";
+import PhoneIcon from "@/components/icons/PhoneIcon";
+import ClockIcon from "@/components/icons/ClockIcon";
 import LocationIcon from "@/public/footer/address.svg";
 import TelephoneIcon from "@/public/TelephoneIcon.png";
 import ContactBanner from "@/public/ContactBanner.png";
+import style from "./contact.module.scss";
+import Button from "@/components/ui/Buttons";
+import Link from "next/link";
 
 const contacts = [
   {
     title: "شماره تماس",
     description: <a href="tel:04133321028">041-33321028</a>,
-    icon: <Image src={PhoneIcon} className="invert w-10" alt="Phone" />,
+    icon: <PhoneIcon />,
   },
   {
     title: "ساعات کاری",
-    description: "شنبه تا چهارشنبه 16 تا 21 پنج‌شنبه‌ها 11تا 15 ",
-    icon: <Image src={ClockIcon} className="w-10 invert" alt="Clock" />,
+    description: (
+      <span>
+        شنبه تا چهارشنبه 16 تا 21 <br /> پنج‌شنبه‌ها 11تا 15{" "}
+      </span>
+    ),
+    icon: <ClockIcon />,
   },
-  // {
-  //   title: "اینستاگرام",
-  //   description: (
-  //     <a href="https://www.instagram.com/dr.soheila.karimi/">
-  //       @dr.soheila.karimi
-  //     </a>
-  //   ),
-  //   icon: <Image className="w-10" src={InstagramIcon} alt="Instagram" />,
-  // },
 ];
 
 function Page() {
   return (
     <main className="mt-20 min-h-screen">
-      <div className="flex pr-5 pt-24 md:pr-20 gap-4 items-center">
-        <Image src={TelephoneIcon} className="w-10 md:w-min" alt="Telephone" />
-        <h1 className="">ارتباط با ما</h1>
+      <div className="md:max-w-7xl mx-auto mr-5 md:mr-auto">
+        <div className="flex pr-5 pt-24 md:pr-20 gap-4 items-center ">
+          <Image
+            src={TelephoneIcon}
+            className="w-10 md:w-min"
+            alt="Telephone"
+          />
+          <h1 className="">ارتباط با ما</h1>
+        </div>
+        <h3 className="md:pr-20 pt-6 pb-24 p-5">
+          برای مشاوره تلفنی اختصاصی و رزرو وقت، کافیست در ساعات کاری با ما تماس
+          بگیرید.
+        </h3>
       </div>
 
-      <h3 className="md:pr-20 pt-6 pb-24 p-5">
-        برای مشاوره تلفنی اختصاصی و رزرو وقت، کافیست در ساعات کاری با ما تماس
-        بگیرید.
-      </h3>
-
-      <section className="before:content-[''] before:absolute relative before:w-full before:h-full before:bg-black before:bg-opacity-50">
+      <section className="">
         <Image
           src={ContactBanner}
-          className="w-full h-[33rem] object-cover object-top "
+          className="w-full object-cover object-top hidden md:block"
           alt="Contact Banner"
         />
       </section>
 
-      <section>
-        <div className="flex justify-center gap-16">
+      <section className="mb-32 md:-mt-24">
+        <div className="flex flex-col items-center md:flex-row justify-center gap-16 mx-5">
           {contacts.map((contact) => (
-            <div
-              key={contact.title}
-              className="flex max-w-[24rem] px-20 py-8 pb-24 text-center bg-lightBlue 
-                shadow-md shadow-zinc-300 w-full rounded-lg relative -top-24 flex-col gap-2 items-center"
-            >
-              <div className="mb-10">{contact.icon}</div>
-              <h3 className="font-bold text-primary mb-12">{contact.title}</h3>
+            <div key={contact.title} className={style.card}>
+              <div className="w-full flex flex-col items-center">
+                <div className="mb-10">{contact.icon}</div>
+                <h2 className="font-bold text-center">{contact.title}</h2>
+              </div>
               <h5>{contact.description}</h5>
             </div>
           ))}
         </div>
       </section>
 
-      <section className=" flex items-center mt-12">
-        <div className="h-80 text-Neutral pr-20 pt-12 bg-primary w-full">
+      <section className=" flex items-center flex-col md:flex-row">
+        <div className="md:h-80 text-Neutral px-5 md:pr-48 py-12 bg-primary w-full rounded-lg md:rounded-bl-none md:rounded-tl-none md:rounded-tr-lg md:rounded-br-lg">
           <div className="flex gap-2 ">
             <Image src={LocationIcon} className="w-6" alt="Location" />
-            <h3 className="font-bold">آدرس ما</h3>
+            <h2 className="font-bold text-2xl mb-4">آدرس ما</h2>
           </div>
-          <div className="flex gap-2 mt-16">
-            <h5>
+          <div className="flex gap-2 md:mt-16">
+            <h3>
               تبریز، ولیعصر، خیابان پروین اعتصامی، روبروی قنادی پاک، ساختمان
-              پزشکان 1، طبقه هفتم، واحدB
-            </h5>
+              پزشکان 1، طبقه هفتم، واحد B
+            </h3>
           </div>
         </div>
-        <div className="bg-accent relative mr-auto z-0 py-4 pr-4 max-w-[75rem] w-full rounded-tr-lg rounded-br-lg h-[36.5rem]">
+        <div className="bg-accent relative w-[90%] mx-auto mr-auto z-0 py-4 pr-4 pl-4 md:pl-0 max-w-3xl md:w-full rounded-lg md:rounded-bl-none md:rounded-tl-none md:rounded-tr-lg md:rounded-br-lg md:h-[36.5rem]">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d832.9439167393656!2d46.36135513800614!3d38.06218727627918!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x401a1be87093cfd9%3A0xad3c6b856558b5ba!2z2LPYp9iu2KrZhdin2YYg2b7Ysti02qnYp9mG!5e0!3m2!1sen!2sae!4v1690826504853!5m2!1sen!2sae"
             style={{ border: 0 }}
@@ -97,6 +99,30 @@ function Page() {
           در خارج از ساعات کاری، در اینستاگرام یا واتس‌اپ پیام دهید. همکاران ما
           در اسرع وقت پاسخگوی شما هستند.
         </h3>
+
+        <div className="flex flex-col md:flex-row gap-8 mt-16 w-full justify-center max-w-sm mx-auto">
+          <Link
+            target="_blank"
+            href="https://www.instagram.com/dr.soheila.karimi/"
+          >
+            <Button className="flex w-full">
+              dr.sohaila.karimi
+              <Image
+                src={InstagramIcon}
+                alt="Instagram"
+                style={{ maxWidth: "auto" }}
+                className="w-16 h-16 max-w-none"
+              />
+            </Button>
+          </Link>
+
+          <Link href="https://wa.me/98914133321028">
+            <Button className="flex w-full">
+              041-33321028
+              <WhatsAppIcon className="w-16 h-16" />
+            </Button>
+          </Link>
+        </div>
       </section>
     </main>
   );
